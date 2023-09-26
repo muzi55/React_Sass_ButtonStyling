283e7242d5ce1c2e97a223d826735721e5985d6e
muzi55 <muzi5@kakao.com>
2023-09-27 00:51:00 +0900

    size handler

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,12 +4,13 @@ interface Props {
   className?: string;
   onClick?: () => void;
   children: React.ReactNode;
+  size?: "small" | "medium" | "large";
   color?: "red" | "blue" | "pink";
 }
 
-const Button = ({ className, onClick, color = "blue", children }: Props): JSX.Element => {
+const Button = ({ className, onClick, color = "blue", size = "medium", children }: Props): JSX.Element => {
   return (
-    <button className={`${className} ${color}`} onClick={onClick}>
+    <button className={`${className} ${color} ${size}`} onClick={onClick}>
       {children}
     </button>
   );
