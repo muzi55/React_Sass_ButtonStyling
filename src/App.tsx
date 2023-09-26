e6889dcc8d19ab503308f6767e02328ac4f7611c
muzi55 <muzi5@kakao.com>
2023-09-27 00:47:21 +0900

    버튼 클릭이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import Button from "./components/button/Button";
 
 function App() {
+  const handleBtnConsole = () => {
+    console.log("버튼이 클릭되었습니다");
+  };
+
   return (
     <>
       <h1>Sass(Scss) 사용하기</h1>
-      <Button color="red">전송</Button>
+      <Button onClick={handleBtnConsole} color="red">
+        전송
+      </Button>
     </>
   );
 }
